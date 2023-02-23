26208d7c6f64534cd9b12b9da1ce5cd4a0875f0c
berkaytikenoglu <berkaytikenoglu@gmail.com>
2023-02-24 00:26:20 +0300

    test

---
+++ src/main/java/testplugin/test/Test.java
@@ -0,0 +1,18 @@
+package testplugin.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Test extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        //wewq
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
